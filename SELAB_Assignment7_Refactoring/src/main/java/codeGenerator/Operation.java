3e0f86c1cb80af2d764b1029c5b4be784d26878a
AinazRafiei <ainazrsh2016@gmail.com>
2023-06-07 17:12:47 +0330

    add README.md file

--- SELAB_Assignment7_Refactoring/src/main/java/codeGenerator/Operation.java
+++
@@ -1,9 +0,0 @@
-package codeGenerator;
-
-/**
- * Created by mohammad hosein on 6/27/2015.
- */
-
-public enum Operation {
-    ADD, AND, ASSIGN, EQ, JPF, JP, LT, MULT, NOT, PRINT, SUB
-}
--- SE_LAB6/src/main/java/org/example/AbstractClasses/AbstractGardenCreator.java
+++
@@ -1,8 +0,0 @@
-package org.example.AbstractClasses;
-
-public abstract class AbstractGardenCreator {
-    abstract public AbstractTree createTree(String x);
-    abstract public AbstractFlower createFlower(String x);
-
-    abstract public Object clone();
-}
